f13505aee0b3fdb84fdc13ba6ff2337ef114dd07
bd240897 <bd240897@yandex.ru>
2023-05-28 23:23:07 +0300

    added mock to register

--- frontend/src/pages/RegisterPage/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage/RegisterPage.jsx
@@ -2,12 +2,23 @@ import "./register.sass"
 
 import React from 'react';
 import {Container} from "react-bootstrap";
+import {toast} from "react-toastify";
+import {useNavigate} from "react-router-dom";
 
 function RegisterPage(props) {
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        toast.success("Все ок!");
+        navigate("/")
+    }
+
     return (
         <section className="register-page">
             <div className="container card">
-                <form className="form" method="post">
+                <form className="form" method="post" onSubmit={handleSubmit}>
                     <div className="two-column-wide">
                         <label htmlFor="email">Электронная почта</label>
                         <input type="text" id="email" name="email"/>
